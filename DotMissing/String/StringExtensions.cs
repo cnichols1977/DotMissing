dc158003e54dca5541d5200a77645d86346c4c78
cnichols1977 <cnichols1977@gmail.com>
2015-12-12 10:46:26 -0600

    added IsNullOrWhiteSpace and Repeat

--- DotMissing/String/StringExtensions.cs
+++ DotMissing/String/StringExtensions.cs
@@ -7,6 +7,16 @@ namespace DotMissing.String
 {
     public static class StringExtensions
     {
+        public static bool IsNullOrWhiteSpace(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+
+        public static string Repeat(this string str, int count)
+        {
+            return string.Concat(Enumerable.Repeat(str, count));
+        }
+
         public static ICollection<string> ToChunks(this string str, int chunkSize)
         {
             Collection<string> c = new Collection<string>();
